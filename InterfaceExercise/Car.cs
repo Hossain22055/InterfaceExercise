822b04bc725d7f7dccf54a11a88e38f621930045
Mohammed Hossain <jamalhossain1629@gmail.com>
2021-02-07 22:21:55 -0500

    First Draft

---
+++ InterfaceExercise/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceExercise
+{
+    public class Car : IVehicle, ICompany
+    {
+        public double EngineSize { get; set; } = 4.2;
+        public string Make { get; set; } = "Ford";
+
+        public string Model { get; set; } = "Mustang";
+        public int SeatCount { get; set; } = 4;
+        public string CompantName { get; set; } = "Ford";
+        public string Motto { get; set; } = "Build Ford Tough";
+
+        public void Drive()
+        {
+            Console.WriteLine($"{GetType().Name} now drive forwad");
+        }
+
+        public void Park()
+        {
+            Console.WriteLine();
+        }
+
+        public void Reverse()
+        {
+            Console.WriteLine($"{GetType().Name} now drive revese");
+        }
+    }
+}
